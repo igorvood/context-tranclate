15b4c3a407225b628840a249c3c7d7b046a150d3
vood <igor-vood@gmail.com>
2025-09-19 14:38:11 +0300

    а

--- src/main/kotlin/ru/vood/context/bigDto/AbstractContextParam.kt
+++ src/main/kotlin/ru/vood/context/bigDto/AbstractContextParam.kt
@@ -1,8 +1,6 @@
 package ru.vood.context.bigDto
 
 import arrow.core.Either
-import java.lang.reflect.ParameterizedType
-import java.lang.reflect.Type
 import kotlin.reflect.KFunction
 
 /**
@@ -50,60 +48,6 @@ sealed class AbstractContextParam<out T, E : IEnrichError>() {
 
 
     abstract fun param(): T?
-
-    /**
-     * Проверяет, является ли тип T nullable (объявлен с знаком '?').
-     * Для определения nullability типа используется рефлексия через класс.
-     */
-    val isTNullable by lazy { isTypeNullable() }
-
-    /**
-     * Вспомогательная функция для определения nullability типа через рефлексию.
-     */
-    protected fun isTypeNullable(): Boolean {
-        return try {
-            // Получаем generic superclass
-            val superclass = this.javaClass.genericSuperclass
-            if (superclass is ParameterizedType) {
-                val actualTypeArguments = superclass.actualTypeArguments
-                if (actualTypeArguments.isNotEmpty()) {
-                    val type = actualTypeArguments[0]
-                    // Проверяем, является ли тип nullable
-                    isTypeMarkedNullable(type)
-                } else {
-                    true // Безопасное значение по умолчанию
-                }
-            } else {
-                true // Безопасное значение по умолчанию
-            }
-        } catch (e: Exception) {
-            true // Безопасное значение по умолчанию в случае ошибки
-        }
-    }
-
-    /**
-     * Проверяет, помечен ли тип как nullable.
-     */
-    private fun isTypeMarkedNullable(type: Type): Boolean {
-        return when (type) {
-            is Class<*> -> {
-                // Для Class - проверяем, не является ли он примитивным типом
-                !type.isPrimitive && type != Void.TYPE
-            }
-
-            is ParameterizedType -> {
-                // Для ParameterizedType проверяем raw type
-                val rawType = type.rawType
-                if (rawType is Class<*>) {
-                    !rawType.isPrimitive && rawType != Void.TYPE
-                } else {
-                    true
-                }
-            }
-
-            else -> true // Для WildcardType и других - считаем nullable
-        }
-    }
 
 //    abstract fun<TT> success(
 //        value: TT?,

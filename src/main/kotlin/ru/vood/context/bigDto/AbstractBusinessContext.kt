282a138808b8c3a0c931b4cd1db0d7b46249b79b
vood <igor-vood@gmail.com>
2025-10-03 18:56:11 +0300

    а

--- src/main/kotlin/ru/vood/context/bigDto/AbstractBusinessContext.kt
+++ src/main/kotlin/ru/vood/context/bigDto/AbstractBusinessContext.kt
@@ -3,28 +3,36 @@ package ru.vood.context.bigDto
 import kotlin.reflect.KFunction
 import kotlin.reflect.KProperty1
 
-data class CTXMeta<BC : IBusinessContext<BC>, T : IContextParam?, E : IEnrichError, CP : AbstractContextParam<T, E>>(
+data class CTXMeta<BC : AbstractBusinessContext<BC>, T : IContextParam?, E : IEnrichError, CP : AbstractContextParam<T, E>>(
     val prop: KProperty1<BC, CP>,
     val copyFun: (BC, CP) -> (BC),
     val mustEnrichedAfter: Set<String> = setOf()
 )
 
-infix fun <BC : IBusinessContext<BC>, T : IContextParam?, E : IEnrichError, CP : AbstractContextParam<T, E>> KProperty1<BC, CP>.withCopy(
+infix fun <BC : AbstractBusinessContext<BC>, T : IContextParam?, E : IEnrichError, CP : AbstractContextParam<T, E>> KProperty1<BC, CP>.withCopy(
     fc: (BC, CP) -> (BC)
 ): CTXMeta<BC, T, E, CP> {
     return CTXMeta(this, fc)
 }
 
-infix fun <BC : IBusinessContext<BC>, T : IContextParam?, E : IEnrichError, CP : AbstractContextParam<T, E>> CTXMeta<BC, T, E, CP>.erichedAfther(
+infix fun <BC : AbstractBusinessContext<BC>, T : IContextParam?, E : IEnrichError, CP : AbstractContextParam<T, E>> CTXMeta<BC, T, E, CP>.erichedAfther(
     otherProp: KProperty1<BC, *>
 ): CTXMeta<BC, T, E, CP> {
     require(this.prop.name != otherProp.name) { "property '${this.prop.name}' cannot be enriched after it self" }
     return this.copy(mustEnrichedAfter = this.mustEnrichedAfter.plus(otherProp.name))
 }
 
-interface IBusinessContext<BC : IBusinessContext<BC>> {
+infix fun <BC : AbstractBusinessContext<BC>, T : IContextParam?, E : IEnrichError, CP : AbstractContextParam<T, E>> CTXMeta<BC, T, E, CP>.erichedAfther(
+    otherProps: List<KProperty1<BC, *>>
+): CTXMeta<BC, T, E, CP> = otherProps
+    .fold(this) { acc, otherProp ->
+        acc erichedAfther otherProp
+    }
 
-    val propsCTXMeta: List<CTXMeta<BC, *, *, *>>
+
+abstract class AbstractBusinessContext<BC : AbstractBusinessContext<BC>> {
+
+    abstract val propsCTXMeta: List<CTXMeta<BC, *, *, *>>
 
     val includedContextProperty
         get() = propsCTXMeta.map { it.prop }
@@ -65,21 +73,11 @@ interface IBusinessContext<BC : IBusinessContext<BC>> {
         return filter
     }
 
-
-//    fun <T : IContextParam, E : IEnrichError> enrichOk(
-//        prop: KProperty1<BC, AbstractContextParam<T, E>>,
-//        data: T,
-//        method: KFunction<*>
-//    ): BC {
-//        val meta: CTXMeta<BC, T, E, AbstractContextParam<T, E>> = (propsCTXMetaMap[prop.name] ?: error("this error un imposible")).let { it as CTXMeta<BC, T, E, AbstractContextParam<T, E>> }
-//        val enrichedParam: AbstractContextParam<T, E> = meta.prop.invoke().enrichError(data,method)
-//        return meta.copyFun(this as BC, enrichedParam)
-//    }
-
     fun mutableMethods(): List<Pair<String, MutableMethod>> {
         return includedContextProperty
             .flatMap { prop -> prop.invoke(this as BC).mutableMethods.map { v -> prop.name to v } }
             .sortedBy { it.second.time }
     }
 
+    val mutationInfo by lazy { mutableMethods() }
 }
--- src/test/kotlin/ru/vood/context/bigDto/example/dto/DataApplicationContext.kt
+++ src/test/kotlin/ru/vood/context/bigDto/example/dto/DataApplicationContext.kt
@@ -4,8 +4,6 @@ import kotlinx.serialization.Serializable
 import ru.vood.context.bigDto.*
 import ru.vood.context.bigDto.ImmutableContextParam.Companion.pendingImmutable
 import ru.vood.context.bigDto.MutableContextParam.Companion.pendingMutable
-import kotlin.reflect.KFunction
-import kotlin.reflect.KProperty0
 
 @Serializable
 data class DataApplicationContext(
@@ -15,100 +13,20 @@ data class DataApplicationContext(
     val productInfo: MutableContextParam<ProductInfos, SomeError> = pendingMutable(),
     val participantInfo: ImmutableContextParam<ParticipantInfo?, SomeError> = pendingImmutable(),
     val riskInfo: MutableContextParam<RiskInfo?, SomeError> = pendingMutable(),
-) : IBusinessContext<DataApplicationContext> {
+) : AbstractBusinessContext<DataApplicationContext>() {
 
     override val propsCTXMeta: List<CTXMeta<DataApplicationContext, *, *, *>>
         get() {
             return listOf(
-                DataApplicationContext::dealInfo withCopy { q, w -> q.copy(dealInfo = w) } ,
-                (DataApplicationContext::productInfo withCopy { q, w -> q.copy(productInfo = w) }) erichedAfther DataApplicationContext::dealInfo,
-                DataApplicationContext::participantInfo withCopy { q, w -> q.copy(participantInfo = w) } erichedAfther DataApplicationContext::productInfo,
-                DataApplicationContext::riskInfo withCopy { q, w -> q.copy(riskInfo = w) } erichedAfther DataApplicationContext::participantInfo erichedAfther DataApplicationContext::dealInfo
+                DataApplicationContext::dealInfo withCopy { q, w -> q.copy(dealInfo = w) },
+                DataApplicationContext::productInfo withCopy { q, w -> q.copy(productInfo = w) } erichedAfther DataApplicationContext::dealInfo,
+                DataApplicationContext::participantInfo withCopy { q, w -> q.copy(participantInfo = w) } erichedAfther DataApplicationContext::productInfo erichedAfther DataApplicationContext::dealInfo,
+                DataApplicationContext::riskInfo withCopy { q, w -> q.copy(riskInfo = w) } erichedAfther listOf(
+                    DataApplicationContext::participantInfo,
+                    DataApplicationContext::dealInfo
+                )
             )
         }
 
-//    fun <T : IContextParam, E : IEnrichError> enrichError1(
-//        error: E,
-//        prop: KProperty1<DataApplicationContext, AbstractContextParam<T, E>>,
-//        method: KFunction<*>
-//    ): DataApplicationContext {
-//        val enrichError = prop(this).enrichError(error, method)
-//        val context = when (prop) {
-//            DataApplicationContext::dealInfo -> copy(dealInfo = enrichError as ImmutableContextParam<DealInfo, SomeError>)
-//            DataApplicationContext::productInfo -> copy(productInfo = enrichError as MutableContextParam<ProductInfos, SomeError>)
-//            DataApplicationContext::participantInfo -> copy(participantInfo = enrichError as ImmutableNullableContextParam<ParticipantInfo, SomeError>)
-//            DataApplicationContext::riskInfo -> copy(riskInfo = enrichError as MutableNullableContextParam<RiskInfo, SomeError>)
-//            else -> error("Unknown property: $prop")
-//        }
-//
-//        return context
-//    }
-
-/*
-    fun <T : IContextParam, E : IEnrichError> enrich(
-        data: T?,
-        prop: KProperty1<DataApplicationContext, AbstractContextParam<T, E>>,
-        method: KFunction<*>
-    ): DataApplicationContext {
-        val prop1 = prop(this)
-        val param = when (prop1) {
-            is ImmutableContextParam<T, E> -> {
-//                assertImmutable()
-                prop1.copy(
-                    result = data?.right(),
-                    mutableMethods = prop1.mutableMethods.plus(MutableMethod(method))
-                )
-            }
-
-            is ImmutableNullableContextParam<T, E> -> {
-//                assertImmutable()
-                prop1.copy(
-                    result = data?.right(),
-                    mutableMethods = prop1.mutableMethods.plus(MutableMethod(method))
-                )
-            }
-
-            is MutableContextParam<T, E> -> prop1.copy(
-                result = data?.right(),
-                mutableMethods = prop1.mutableMethods.plus(MutableMethod(method))
-            )
-
-            is MutableNullableContextParam<T, E> -> prop1.copy(
-                result = data?.right(),
-                mutableMethods = prop1.mutableMethods.plus(MutableMethod(method))
-            )
-        }
-
-
-
-        TODO()
-//        return this.dealInfo.success(dealInfo, method)
-//            .let { this.copy(dealInfo = it) }
-    }
-*/
-
-//    fun enrich(dealInfo: DealInfo, method: KFunction<*>): DataApplicationContext {
-//        val property: KProperty0<ImmutableContextParam<DealInfo, SomeError>> = this::dealInfo
-//        return this.dealInfo.enrichOk(dealInfo, method)
-//            .let { this.copy(dealInfo = it) }
-//    }
-//
-//    fun enrich(productInfo: Set<ProductInfo>, method: KFunction<*>): DataApplicationContext {
-//        require(dealInfo.isReceived()) { "не могу принять ProductInfo, он должен быть принят после dealInfo" }
-//        return this.productInfo.success(ProductInfos(productInfo), method)
-//            .let { this.copy(productInfo = it) }
-//    }
-//
-//    fun enrich(participantInfo: ParticipantInfo, method: KFunction<*>): DataApplicationContext {
-//        return this.participantInfo.enrichOk(participantInfo, method)
-//            .let { this.copy(participantInfo = it) }
-//    }
-//
-//    fun enrich(riskInfo: RiskInfo, method: KFunction<*>): DataApplicationContext {
-//        return this.riskInfo.success(riskInfo, method)
-//            .let { this.copy(riskInfo = it) }
-//    }
-
-    val mutationInfo by lazy { mutableMethods() }
 
 }

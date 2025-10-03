06660cc33b5997b594a4c3e268332f249db78a10
vood <igor-vood@gmail.com>
2025-10-03 20:38:08 +0300

    а

--- src/main/kotlin/ru/vood/context/bigDto/ImmutableContextParam.kt
+++ src/main/kotlin/ru/vood/context/bigDto/ImmutableContextParam.kt
@@ -8,6 +8,12 @@ import kotlinx.serialization.Serializable
  * Неизменяемый (immutable) параметр контекста, который гарантированно содержит не-null значение
  * после успешной обработки. Реализует функциональный подход с использованием Either для обработки ошибок.
  *
+ * Основные характеристики:
+ * - Значение устанавливается только один раз
+ * - После установки значения параметр становится "замороженным"
+ * - Поддерживает трассировку методов, которые участвовали в установке значения
+ * - Использует Either для функциональной обработки успехов и ошибок
+ *
  * @param T тип параметра контекста, реализующий [IContextParam]
  * @param E тип ошибки, реализующий [IEnrichError]
  * @property result результат обработки параметра в виде [Either], содержащий либо ошибку [E], либо значение параметра [T]
@@ -25,14 +31,27 @@ data class ImmutableContextParam<T : IContextParam?, E : IEnrichError>(
     /**
      * Всегда возвращает false, так как данный параметр является неизменяемым.
      * После установки значения не может быть изменен.
+     *
+     * Это ключевое отличие от MutableContextParam - гарантирует, что значение
+     * устанавливается единожды и остается постоянным throughout жизненного цикла.
      */
     override val mutableParam: Boolean
         get() = false
 
     companion object {
+        /**
+         * Создает новый экземпляр ImmutableContextParam в состоянии "ожидания".
+         * Параметр еще не имеет значения и готов к последующей установке через enrichOk/enrichError.
+         *
+         * @return ImmutableContextParam<T, E> с пустым результатом и пустым списком методов
+         *
+         * Пример использования:
+         * ```
+         * val pendingParam = ImmutableContextParam.pendingImmutable<UserContext, ValidationError>()
+         * ```
+         */
         fun <T : IContextParam?, E : IEnrichError> pendingImmutable(): ImmutableContextParam<T, E> {
             return ImmutableContextParam()
         }
-
     }
 }
